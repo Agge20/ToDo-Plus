558b72369bdc50c392649a865326e31eb6380e95
august-040 <august@040.se>
2022-09-01 19:16:23 +0200

    Initial commit

---
+++ src/firebase/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAHFU5nTqx5nxCalNsLRlbRhjlA8YcQun8",
+    authDomain: "todo-plus-4e167.firebaseapp.com",
+    projectId: "todo-plus-4e167",
+    storageBucket: "todo-plus-4e167.appspot.com",
+    messagingSenderId: "90718957856",
+    appId: "1:90718957856:web:80e742e8a9857e36422745",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
