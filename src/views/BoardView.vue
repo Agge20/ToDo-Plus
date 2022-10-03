34e92e1b5cfb3063a4c58a2663a2d2f161ecdd31
august <augustraro112@hotmail.com>
2022-10-03 21:50:15 +0200

    Adding welcome component

---
+++ src/views/BoardView.vue
@@ -0,0 +1,18 @@
+<template>
+    <section>
+        <welcome />
+    </section>
+</template>
+
+<script lang="ts">
+    // vue imports
+    import { defineComponent } from "vue";
+
+    // components
+    import Welcome from "../components/board/Welcome.vue";
+
+    export default defineComponent({
+        components: { Welcome },
+        setup() {},
+    });
+</script>
