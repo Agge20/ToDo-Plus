3f0325676d02162ec347b5f70d0fcc4b13e352fc
august <augustraro112@hotmail.com>
2022-10-16 16:09:35 +0200

    Added edit todo button and completed todo button

---
+++ src/components/buttons/PencilButton.vue
@@ -0,0 +1,19 @@
+<template>
+    <button class="rounded-xl bg-ct-white p-2 opacity-80">
+        <pencil-square-icon />
+    </button>
+</template>
+
+<script lang="ts">
+    // vue imports
+    import { defineComponent } from "vue";
+
+    // svg
+    import PencilSquareIcon from "../icons/PencilSquareIcon.vue";
+
+    export default defineComponent({
+        components: {
+            PencilSquareIcon,
+        },
+    });
+</script>
